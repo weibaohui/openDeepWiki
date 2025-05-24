6238fe1f813e566de3b5802bf1b766197736dfc5
weibaohui <weibaohui@yeah.net>
2025-05-24 17:21:16 +0800

    refactor: 移除冗余代码并优化OpenAIClient的历史记录管理逻辑

--- pkg/ai/openai.go
+++ pkg/ai/openai.go
@@ -1,14 +1,160 @@
-/*
-Copyright 2023 The K8sGPT Authors.
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-    http://www.apache.org/licenses/LICENSE-2.0
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package ai
+
+import (
+	"context"
+	"errors"
+	"net/http"
+	"net/url"
+
+	"github.com/sashabaranov/go-openai"
+	"github.com/weibaohui/openDeepWiki/pkg/comm/utils"
+	"k8s.io/klog/v2"
+)
+
+const openAIClientName = "openai"
+
+type OpenAIClient struct {
+	nopCloser
+	client      *openai.Client
+	model       string
+	temperature float32
+	topP        float32
+	tools       []openai.Tool
+	maxHistory  int32
+	memory      *memoryService
+
+	// organizationId string
+}
+
+func (c *OpenAIClient) SetTools(tools []openai.Tool) {
+	c.tools = tools
+}
+
+func (c *OpenAIClient) Configure(config IAIConfig) error {
+	klog.V(6).Infof("OpenAIClient Configure \n %s \n", utils.ToJSON(config))
+	token := config.GetPassword()
+	cfg := openai.DefaultConfig(token)
+	orgId := config.GetOrganizationId()
+	proxyEndpoint := config.GetProxyEndpoint()
+
+	baseURL := config.GetBaseURL()
+	if baseURL != "" {
+		cfg.BaseURL = baseURL
+	}
+
+	transport := &http.Transport{}
+	if proxyEndpoint != "" {
+		proxyUrl, err := url.Parse(proxyEndpoint)
+		if err != nil {
+			return err
+		}
+		transport.Proxy = http.ProxyURL(proxyUrl)
+	}
+
+	if orgId != "" {
+		cfg.OrgID = orgId
+	}
+
+	customHeaders := config.GetCustomHeaders()
+	cfg.HTTPClient = &http.Client{
+		Transport: &OpenAIHeaderTransport{
+			Origin:  transport,
+			Headers: customHeaders,
+		},
+	}
+
+	client := openai.NewClientWithConfig(cfg)
+	if client == nil {
+		return errors.New("error creating OpenAI client")
+	}
+	c.client = client
+	c.model = config.GetModel()
+	c.temperature = config.GetTemperature()
+	c.topP = config.GetTopP()
+	c.maxHistory = config.GetMaxHistory()
+	c.memory = NewMemoryService()
+	return nil
+}
+
+func (c *OpenAIClient) GetCompletion(ctx context.Context, contents ...any) (string, error) {
+	c.fillChatHistory(ctx, contents)
+
+	// Create a completion request
+	resp, err := c.client.CreateChatCompletion(ctx,
+		openai.ChatCompletionRequest{
+			Model:    c.model,
+			Messages: c.GetHistory(ctx),
+		})
+	if err != nil {
+		return "", err
+	}
+	return resp.Choices[0].Message.Content, nil
+}
+func (c *OpenAIClient) GetCompletionWithTools(ctx context.Context, contents ...any) ([]openai.ToolCall, string, error) {
+
+	// Create a completion request
+	c.fillChatHistory(ctx, contents)
+	resp, err := c.client.CreateChatCompletion(ctx,
+		openai.ChatCompletionRequest{
+			Model:       c.model,
+			Messages:    c.GetHistory(ctx),
+			Temperature: c.temperature,
+			TopP:        c.topP,
+			Tools:       c.tools,
+		})
+	if err != nil {
+		return nil, "", err
+	}
+	return resp.Choices[0].Message.ToolCalls, resp.Choices[0].Message.Content, nil
+}
+
+func (c *OpenAIClient) GetStreamCompletion(ctx context.Context, contents ...any) (*openai.ChatCompletionStream, error) {
+	c.fillChatHistory(ctx, contents)
+	stream, err := c.client.CreateChatCompletionStream(ctx, openai.ChatCompletionRequest{
+		Model:       c.model,
+		Messages:    c.GetHistory(ctx),
+		Temperature: c.temperature,
+		TopP:        c.topP,
+		Stream:      true,
+	})
+	return stream, err
+}
+func (c *OpenAIClient) GetStreamCompletionWithTools(ctx context.Context, contents ...any) (*openai.ChatCompletionStream, error) {
+	c.fillChatHistory(ctx, contents)
+	history := c.GetHistory(ctx)
+	stream, err := c.client.CreateChatCompletionStream(ctx, openai.ChatCompletionRequest{
+		Model:    c.model,
+		Messages: history,
+		Tools:    c.tools,
+		Stream:   true,
+	})
+	klog.V(6).Infof("GetStreamCompletionWithTools 携带 history length: %d", len(history))
+	klog.V(6).Infof("GetStreamCompletionWithTools c.history: %v", utils.ToJSON(history))
+	return stream, err
+}
+
+func (c *OpenAIClient) GetName() string {
+	return openAIClientName
+}
+
+// OpenAIHeaderTransport is an http.RoundTripper that adds the given headers to each request.
+type OpenAIHeaderTransport struct {
+	Origin  http.RoundTripper
+	Headers []http.Header
+}
+
+// RoundTrip implements the http.RoundTripper interface.
+func (t *OpenAIHeaderTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+	// Clone the request to avoid modifying the original request
+	clonedReq := req.Clone(req.Context())
+	for _, header := range t.Headers {
+		for key, values := range header {
+			// Possible values per header:  RFC 2616
+			for _, value := range values {
+				clonedReq.Header.Add(key, value)
+			}
+		}
+	}
+
+	return t.Origin.RoundTrip(clonedReq)
+}
--- pkg/ai/openai_history.go
+++ pkg/ai/openai_history.go
@@ -2,9 +2,6 @@ package ai
 
 import (
 	"context"
-	"errors"
-	"net/http"
-	"net/url"
 
 	"github.com/duke-git/lancet/v2/slice"
 	"github.com/sashabaranov/go-openai"
@@ -13,182 +10,6 @@ import (
 	"github.com/weibaohui/openDeepWiki/pkg/models"
 	"k8s.io/klog/v2"
 )
-
-const openAIClientName = "openai"
-
-type OpenAIClient struct {
-	nopCloser
-	client      *openai.Client
-	model       string
-	temperature float32
-	topP        float32
-	tools       []openai.Tool
-	maxHistory  int32
-	memory      *memoryService
-
-	// organizationId string
-}
-
-func (c *OpenAIClient) SetTools(tools []openai.Tool) {
-	c.tools = tools
-}
-
-func (c *OpenAIClient) Configure(config IAIConfig) error {
-	klog.V(6).Infof("OpenAIClient Configure \n %s \n", utils.ToJSON(config))
-	token := config.GetPassword()
-	cfg := openai.DefaultConfig(token)
-	orgId := config.GetOrganizationId()
-	proxyEndpoint := config.GetProxyEndpoint()
-
-	baseURL := config.GetBaseURL()
-	if baseURL != "" {
-		cfg.BaseURL = baseURL
-	}
-
-	transport := &http.Transport{}
-	if proxyEndpoint != "" {
-		proxyUrl, err := url.Parse(proxyEndpoint)
-		if err != nil {
-			return err
-		}
-		transport.Proxy = http.ProxyURL(proxyUrl)
-	}
-
-	if orgId != "" {
-		cfg.OrgID = orgId
-	}
-
-	customHeaders := config.GetCustomHeaders()
-	cfg.HTTPClient = &http.Client{
-		Transport: &OpenAIHeaderTransport{
-			Origin:  transport,
-			Headers: customHeaders,
-		},
-	}
-
-	client := openai.NewClientWithConfig(cfg)
-	if client == nil {
-		return errors.New("error creating OpenAI client")
-	}
-	c.client = client
-	c.model = config.GetModel()
-	c.temperature = config.GetTemperature()
-	c.topP = config.GetTopP()
-	c.maxHistory = config.GetMaxHistory()
-	c.memory = NewMemoryService()
-	return nil
-}
-
-func (c *OpenAIClient) GetCompletion(ctx context.Context, contents ...any) (string, error) {
-	c.fillChatHistory(ctx, contents)
-
-	// Create a completion request
-	resp, err := c.client.CreateChatCompletion(ctx,
-		openai.ChatCompletionRequest{
-			Model:    c.model,
-			Messages: c.GetHistory(ctx),
-		})
-	if err != nil {
-		return "", err
-	}
-	return resp.Choices[0].Message.Content, nil
-}
-func (c *OpenAIClient) GetCompletionWithTools(ctx context.Context, contents ...any) ([]openai.ToolCall, string, error) {
-
-	// Create a completion request
-	c.fillChatHistory(ctx, contents)
-	resp, err := c.client.CreateChatCompletion(ctx,
-		openai.ChatCompletionRequest{
-			Model:       c.model,
-			Messages:    c.GetHistory(ctx),
-			Temperature: c.temperature,
-			TopP:        c.topP,
-			Tools:       c.tools,
-		})
-	if err != nil {
-		return nil, "", err
-	}
-	return resp.Choices[0].Message.ToolCalls, resp.Choices[0].Message.Content, nil
-}
-
-func (c *OpenAIClient) GetStreamCompletion(ctx context.Context, contents ...any) (*openai.ChatCompletionStream, error) {
-	c.fillChatHistory(ctx, contents)
-	stream, err := c.client.CreateChatCompletionStream(ctx, openai.ChatCompletionRequest{
-		Model:       c.model,
-		Messages:    c.GetHistory(ctx),
-		Temperature: c.temperature,
-		TopP:        c.topP,
-		Stream:      true,
-	})
-	return stream, err
-}
-func (c *OpenAIClient) GetStreamCompletionWithTools(ctx context.Context, contents ...any) (*openai.ChatCompletionStream, error) {
-	c.fillChatHistory(ctx, contents)
-	history := c.GetHistory(ctx)
-	stream, err := c.client.CreateChatCompletionStream(ctx, openai.ChatCompletionRequest{
-		Model:    c.model,
-		Messages: history,
-		Tools:    c.tools,
-		Stream:   true,
-	})
-	klog.V(6).Infof("GetStreamCompletionWithTools 携带 history length: %d", len(history))
-	klog.V(6).Infof("GetStreamCompletionWithTools c.history: %v", utils.ToJSON(history))
-	return stream, err
-}
-
-func (c *OpenAIClient) GetName() string {
-	return openAIClientName
-}
-
-// OpenAIHeaderTransport is an http.RoundTripper that adds the given headers to each request.
-type OpenAIHeaderTransport struct {
-	Origin  http.RoundTripper
-	Headers []http.Header
-}
-
-// RoundTrip implements the http.RoundTripper interface.
-func (t *OpenAIHeaderTransport) RoundTrip(req *http.Request) (*http.Response, error) {
-	// Clone the request to avoid modifying the original request
-	clonedReq := req.Clone(req.Context())
-	for _, header := range t.Headers {
-		for key, values := range header {
-			// Possible values per header:  RFC 2616
-			for _, value := range values {
-				clonedReq.Header.Add(key, value)
-			}
-		}
-	}
-
-	return t.Origin.RoundTrip(clonedReq)
-}
-
-func (c *OpenAIClient) SaveAIHistory(ctx context.Context, contents string) {
-	val := ctx.Value(constants.RepoName)
-	if repoName, ok := val.(string); ok {
-		c.memory.AppendRepoHistory(repoName, openai.ChatCompletionMessage{
-			Role:    openai.ChatMessageRoleAssistant,
-			Content: contents,
-		})
-	} else {
-		klog.Warningf("SaveAIHistory content but repo not found: %s", contents)
-	}
-}
-
-func (c *OpenAIClient) GetHistory(ctx context.Context) []openai.ChatCompletionMessage {
-	val := ctx.Value(constants.RepoName)
-	if repoName, ok := val.(string); ok {
-		return c.memory.GetRepoHistory(repoName)
-	}
-	return make([]openai.ChatCompletionMessage, 0)
-}
-
-func (c *OpenAIClient) ClearHistory(ctx context.Context) error {
-	val := ctx.Value(constants.RepoName)
-	if repoName, ok := val.(string); ok {
-		c.memory.ClearRepoHistory(repoName)
-	}
-	return nil
-}
 
 func (c *OpenAIClient) fillChatHistory(ctx context.Context, contents ...any) {
 
@@ -243,10 +64,7 @@ func (c *OpenAIClient) fillChatHistory(ctx context.Context, contents ...any) {
 	systemPrompt := ctx.Value(constants.SystemPrompt).(string)
 	if systemPrompt != "" {
 		system := slice.Filter(history, func(index int, item openai.ChatCompletionMessage) bool {
-			if item.Role == openai.ChatMessageRoleSystem {
-				return true
-			}
-			return false
+			return item.Role == openai.ChatMessageRoleSystem
 		})
 
 		if len(system) == 0 {
@@ -263,4 +81,32 @@ func (c *OpenAIClient) fillChatHistory(ctx context.Context, contents ...any) {
 	repoName := ctx.Value(constants.RepoName).(string)
 	c.memory.SetRepoHistory(repoName, history)
 
+}
+
+func (c *OpenAIClient) SaveAIHistory(ctx context.Context, contents string) {
+	val := ctx.Value(constants.RepoName)
+	if repoName, ok := val.(string); ok {
+		c.memory.AppendRepoHistory(repoName, openai.ChatCompletionMessage{
+			Role:    openai.ChatMessageRoleAssistant,
+			Content: contents,
+		})
+	} else {
+		klog.Warningf("SaveAIHistory content but repo not found: %s", contents)
+	}
+}
+
+func (c *OpenAIClient) GetHistory(ctx context.Context) []openai.ChatCompletionMessage {
+	val := ctx.Value(constants.RepoName)
+	if repoName, ok := val.(string); ok {
+		return c.memory.GetRepoHistory(repoName)
+	}
+	return make([]openai.ChatCompletionMessage, 0)
+}
+
+func (c *OpenAIClient) ClearHistory(ctx context.Context) error {
+	val := ctx.Value(constants.RepoName)
+	if repoName, ok := val.(string); ok {
+		c.memory.ClearRepoHistory(repoName)
+	}
+	return nil
 }
